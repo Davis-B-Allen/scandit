d7179499416adffbbcfee40221b57d8f04312ac6
Davis Allen <davis.b.allen@gmail.com>
2019-11-11 21:28:06 -0500

    Basic service comms on PCF

---
+++ post-service/src/main/java/com/example/postservice/PostApiApplication.java
@@ -0,0 +1,41 @@
+package com.example.postservice;
+
+import com.example.postservice.client.AuthClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients("com.example.postservice.client")
+@RestController
+public class PostApiApplication {
+
+    @Autowired
+    AuthClient authClient;
+
+    @RequestMapping("/")
+    public String home() {
+        return "some posts";
+    }
+
+    @GetMapping("/myposts")
+    public String myPosts() {
+        String auth = authClient.getAuth();
+        if (auth.equals("valid auth")) {
+            return "my posts";
+        }
+        return "NOT AUTHED";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostApiApplication.class, args);
+	}
+
+}
