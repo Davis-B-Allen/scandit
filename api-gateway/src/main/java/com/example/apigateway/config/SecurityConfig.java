598f5f42610b5ff028ac9316cc6cb86d6f83c675
aridoza <mendozaarielr@gmail.com>
2019-11-13 19:26:37 -0500

    auth working in posts and userRole

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -1,45 +1,26 @@
-package com.example.authservice.config;
+package com.example.apigateway.config;
 
-import com.example.authservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserService userService;
-
-    @Autowired
     JwtRequestFilter jwtRequestFilter;
-
-    @Bean("encoder")
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
-    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/signup/**", "/login/**", "/posts/**", "/auth/").permitAll()
+                .antMatchers("/signup/**", "/login/**", "/posts/**", "/auth/", "/").permitAll()
                 .antMatchers("/post/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
